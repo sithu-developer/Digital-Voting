238e2f43e03f5b82e25714a1a2f70d28e4d27579
unknown <sithunaing8209@gmail.com>
2025-05-29 21:02:00 +0630

    last

--- src/pages/api/vote/index.ts
+++ src/pages/api/vote/index.ts
@@ -18,7 +18,6 @@ export default async function handler(
         const exitStudent = await prisma.user.findUnique({ where : { id : userId }});
         if(!exitStudent) return res.status(400).send("Bad request");
         const isAlreadyVote = await prisma.votes.findFirst({ where : { AND : { studentId , userId }}});
-        console.log(isAlreadyVote)
         if(isAlreadyVote) return res.status(400).send("Already Voted");
         const newVote = await prisma.votes.create({ data : { studentId , userId }});
         return res.status(200).json({ newVote })
