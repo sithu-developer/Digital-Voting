ea5f1a4863d576c4f26dc0c2873d89be2fbccdc5
unknown <sithunaing8209@gmail.com>
2025-12-20 21:35:49 +0630

    render url error fixed

--- src/util/prisma.ts
+++ src/util/prisma.ts
@@ -18,6 +18,12 @@ import { PrismaClient } from './../../generated/prisma/client'
 const connectionString = `${process.env.DATABASE_URL}`
 
 const adapter = new PrismaPg({ connectionString })
-const prisma = new PrismaClient({ adapter })
+// const prisma = new PrismaClient({ adapter })
+
+const globalForPrisma = globalThis as unknown as {
+  prisma?: PrismaClient;
+};
+
+const prisma = globalForPrisma.prisma ?? new PrismaClient({ adapter })
 
 export { prisma }
