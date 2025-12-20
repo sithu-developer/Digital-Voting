3ffd5f8973198a59093e5754afea60aa8fe3ade7
unknown <sithunaing8209@gmail.com>
2025-12-20 19:46:05 +0630

    prisma export fixed

--- src/util/prisma.ts
+++ src/util/prisma.ts
@@ -1,12 +1,23 @@
-import { PrismaClient } from "../../generated/prisma";
+// import { PrismaClient } from "../../generated/prisma";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma?: PrismaClient;
-};
+// const globalForPrisma = globalThis as unknown as {
+//   prisma?: PrismaClient;
+// };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient()
+// export const prisma = globalForPrisma.prisma ?? new PrismaClient()
 
 
-if (process.env.NODE_ENV !== 'production') {
-  globalForPrisma.prisma = prisma;
-}
+// if (process.env.NODE_ENV !== 'production') {
+//   globalForPrisma.prisma = prisma;
+// }
+
+import "dotenv/config";
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from '../generated/prisma/client'
+
+const connectionString = `${process.env.DATABASE_URL}`
+
+const adapter = new PrismaPg({ connectionString })
+const prisma = new PrismaClient({ adapter })
+
+export { prisma }
